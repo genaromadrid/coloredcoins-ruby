bf32586b4fde996793c930417e2f82583bb0d93a
Genaro Madrid <genmadrid@gmail.com>
2016-02-23 22:29:38 -0600

    add self.any_method functionality

--- lib/coloredcoins.rb
+++ lib/coloredcoins.rb
@@ -1,4 +1,19 @@
 require 'coloredcoins/version'
 
 module Coloredcoins
+  autoload :API,        'coloredcoins/api'
+  autoload :Connection, 'coloredcoins/connection'
+
+  attr_writer :api
+
+  API_VERSION = 'v3'
+  BLOCK_CHAIN = 'mainnet'
+
+  def self.api
+    @api ||= API.new(network:BLOCK_CHAIN, api_version:API_VERSION)
+  end
+
+  def self.method_missing(sym, *args, &block)
+    api.send(sym, *args, &block)
+  end
 end
