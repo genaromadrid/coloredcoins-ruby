42027305e546d7e488f4f952010009cd12e21ce2
Genaro Madrid <genmadrid@gmail.com>
2016-02-25 12:44:44 -0600

    fix assignment branch condition

--- lib/coloredcoins/multisig_tx.rb
+++ lib/coloredcoins/multisig_tx.rb
@@ -12,17 +12,11 @@ module Coloredcoins
       check
       key = build_key(key)
       tx.inputs.each_with_index do |input, i|
-        sig_hash    = tx.signature_hash_for_input(i, redeem_script)
-        sigs        = build_sigs(key, sig_hash)
-        initial_script_sig = if input.script_sig.empty?
-            Bitcoin::Script.to_p2sh_multisig_script_sig(redeem_script)
-          else
-            input.script_sig
-          end
-        script_sig  = build_script_sig(sigs, sig_hash, initial_script_sig)
+        sig_hash   = tx.signature_hash_for_input(i, redeem_script)
+        sigs       = build_sigs(key, sig_hash)
+        initial    = input.script_sig.or(initial_script)
 
-        input.script_sig = script_sig
-        raise Coloredcoins::InvalidSignatureError unless valid_sig?(i, script_sig)
+        input.script_sig = build_script_sig(sigs, sig_hash, initial)
       end
       true
     end
@@ -37,11 +31,15 @@ module Coloredcoins
 
   private
 
-    def build_script_sig(sigs, sig_hash, initial_script_sig)
+    def initial_script
+      @initial_script ||= Bitcoin::Script.to_p2sh_multisig_script_sig(redeem_script)
+    end
+
+    def build_script_sig(sigs, sig_hash, initial)
       sigs.each do |sig|
-        Bitcoin::Script.add_sig_to_multisig_script_sig(sig, initial_script_sig)
+        Bitcoin::Script.add_sig_to_multisig_script_sig(sig, initial)
       end
-      Bitcoin::Script.sort_p2sh_multisig_signatures(initial_script_sig, sig_hash)
+      Bitcoin::Script.sort_p2sh_multisig_signatures(initial, sig_hash)
     end
 
     def valid_sig?(i, script)
@@ -54,5 +52,11 @@ module Coloredcoins
         raise ArgumentError, 'Set "pub_keys" or "redeem_script" before signing'
       end
     end
+  end
+end
+
+class String
+  def or(what)
+    strip.empty? ? what : self
   end
 end
