294607709d121c38521d78f9e716c0ba510c7e50
Genaro Madrid <genmadrid@gmail.com>
2016-02-25 19:21:28 -0600

    generate coverage in /coverage instead of tmp

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,7 @@
 require 'simplecov'
 SimpleCov.start do
-  coverage_dir 'tmp/coverage'
   add_filter '/spec/'
-
-  SimpleCov.at_exit do
-    SimpleCov.result.format!
-    system('open tmp/coverage/index.html') if RUBY_PLATFORM['darwin']
-  end
-end if ENV['COVERAGE']
+end
 
 require 'pry'
 require 'rubygems'
